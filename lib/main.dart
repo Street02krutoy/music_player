bd00d697a6447e3f41aed4a1d719de28e9d1f782
srit <masaanekdot@gmail.com>
2024-01-29 19:09:36 +0300

    refucktor (now tested SADGE)

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:music_player/config.dart';
 import 'package:music_player/no_track_widget.dart';
 import 'package:music_player/now_playing.dart';
-import 'package:music_player/track.dart';
 import 'package:music_player/track_widget.dart';
-import 'package:http/http.dart' as http;
-import 'package:music_player/utils/upload.dart';
-import 'dart:convert';
+import 'package:music_player/utils/api/fetch.dart';
+import 'package:music_player/utils/api/upload.dart';
 import 'dart:developer' as dev;
 
 void main() {
@@ -39,47 +36,20 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  List<Track> tracks = List.empty();
-
-  Future<String> fetchTracks() async {
-    return http.get(
-      Uri.parse("${Config.URL}/get"),
-      headers: <String, String>{
-        'Content-Type': 'application/json',
-      },
-    ).then((value) {
-      dev.log(value.body);
-      List fetchedTracks = List.from(jsonDecode(value.body));
-      List<Track> trks = List.empty(growable: true);
-      if (fetchedTracks.isEmpty) return "1";
-      for (var i = 0; i < fetchedTracks.length; i++) {
-        trks.add(Track(
-            name: fetchedTracks[i]["name"],
-            duration: Duration(seconds: fetchedTracks[i]["duration"]),
-            netUrl: "${Config.URL}${fetchedTracks[i]["url"]}"));
-      }
-      tracks = trks;
-      return "0";
-    });
-  }
-
-  Future<String> _future = Future(
-    () => "",
-  );
-
   @override
   void initState() {
     super.initState();
-    _future = fetchTracks();
+    _fetchTrackService = FetchTrackService();
   }
 
-  int playing = 0;
+  late final FetchTrackService _fetchTrackService;
+  late UploadTrackService _uploadTrackService;
 
   @override
   Widget build(BuildContext context) {
     final colorTheme = Theme.of(context).colorScheme;
 
-    UploadTrackService uploadTrackService = UploadTrackService(context);
+    _uploadTrackService = UploadTrackService(context);
 
     return Scaffold(
       appBar: AppBar(
@@ -88,11 +58,11 @@ class _MyHomePageState extends State<MyHomePage> {
         shadowColor: colorTheme.shadow,
         actions: [
           TextButton(
-              onPressed: () => uploadTrackService.showUploadDialog(context),
+              onPressed: () => _uploadTrackService.showUploadDialog(context),
               child: const Text("Upload")),
           TextButton(
               onPressed: () {
-                fetchTracks();
+                _fetchTrackService.refresh();
                 setState(() {});
               },
               child: const Text("Reload"))
@@ -103,54 +73,64 @@ class _MyHomePageState extends State<MyHomePage> {
           Container(
             color: Colors.white,
             child: FutureBuilder<String>(
-              future: _future, // a previously-obtained Future<String> or null
+              future: _fetchTrackService
+                  .future, // a previously-obtained Future<String> or null
               builder: (BuildContext context, AsyncSnapshot<String> snapshot) {
-                return snapshot.data == "0"
-                    ? tracks.isEmpty
-                        ? const Text("")
-                        : ListView.builder(
-                            scrollDirection: Axis.vertical,
-                            shrinkWrap: true,
-                            itemCount: tracks.length,
-                            itemBuilder: (BuildContext context, int index) {
-                              return ElevatedButton(
-                                  onPressed: () {
-                                    setState(() {
-                                      playing = index;
-                                      tracks[playing].play();
-                                    });
-                                    String trk = tracks[playing].name;
+                switch (snapshot.data) {
+                  case "0":
+                    return ListView.builder(
+                      scrollDirection: Axis.vertical,
+                      shrinkWrap: true,
+                      itemCount: _fetchTrackService.tracks.length,
+                      itemBuilder: (BuildContext context, int index) {
+                        return ElevatedButton(
+                            onPressed: () {
+                              setState(() {
+                                _fetchTrackService.playing = index;
+                                _fetchTrackService
+                                    .tracks[_fetchTrackService.playing]
+                                    .play();
+                              });
+                              String trk = _fetchTrackService
+                                  .tracks[_fetchTrackService.playing].name;
 
-                                    dev.log("$trk $playing",
-                                        name: "Now playing");
-                                  },
-                                  child: TrackWidget(
-                                    track: tracks[index],
-                                  ));
+                              dev.log("$trk ${_fetchTrackService.playing}",
+                                  name: "Now playing");
                             },
-                          )
-                    : ElevatedButton(
-                        onPressed: () {
-                          fetchTracks();
-                        },
-                        child: const Text("Refresh"));
+                            child: TrackWidget(
+                              track: _fetchTrackService.tracks[index],
+                            ));
+                      },
+                    );
+                  default:
+                    return const Text("");
+                }
               },
             ),
           ),
           const Spacer(),
           FutureBuilder<String>(
-              future: _future,
+              future: _fetchTrackService.future,
               builder: (BuildContext context, AsyncSnapshot<String> snapshot) {
-                return snapshot.data == "0"
-                    ? NowPlayingWidget(
-                        track: tracks[playing],
+                switch (snapshot.data) {
+                  case "0":
+                    if (_fetchTrackService.tracks.isNotEmpty) {
+                      return NowPlayingWidget(
+                        track: _fetchTrackService
+                            .tracks[_fetchTrackService.playing],
                         notifyParent: () {
                           setState(() {});
                         },
-                      )
-                    : snapshot.data == "1"
-                        ? const NoTrackWidget(name: "No tracks")
-                        : const NoTrackWidget(name: "Network error");
+                      );
+                    }
+                    return const NoTrackWidget(name: "No tracks");
+                  case "1":
+                    return const NoTrackWidget(name: "Error: No tracks");
+                  case "2":
+                    return const NoTrackWidget(name: "Network error");
+                  default:
+                    return const NoTrackWidget(name: "Unexpected error");
+                }
               })
         ],
       ),
